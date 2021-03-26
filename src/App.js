afca44d1667e5c90641cb8504a3fb9d433353ef2
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2021-03-26 11:02:22 +0300

    add switch tag

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Navbar from './components/Navbar'
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
@@ -10,10 +10,12 @@ function App() {
       <Router>
         <div className="App">
           <Navbar />
-          <Route exact path='/' component={Home} />
-          <Route path='/about' component={About} />
-          <Route path='/contact' component={Contact} />
-          <Route path="/:post_id" component={Post} />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+            <Route path="/:post_id" component={Post} />
+          </Switch>
         </div>
       </Router>
    
